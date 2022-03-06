b69f578ebc79e2038ea64f1fde42f474b82998e4
Arsalan <arsalan.firoozi@gmail.com>
2022-03-06 11:34:06 +0330

    .

---
+++ src/Student.java
@@ -0,0 +1,11 @@
+public class Student {
+    String Name = "Unkown";
+    String Familyname = "Unknowny";
+    String National_ID = "0";
+    String Student_ID = "0";
+    String Field = "EE";
+    
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
