155d20af0475fd3570d1b4c389b478a541d65d54
SMAHashemi <35628995+SMAHashemi@users.noreply.github.com>
2022-04-02 16:17:45 -0700

    classes and methods added

--- src/Edge.java
+++ src/Edge.java
@@ -1,33 +1,22 @@
 import java.util.*;
 
-public class Edge {
-    public Boolean directed = null;
-    public Node start = null;
-    public Node end = null;
-    public ArrayList<Node> Nodes = new ArrayList<Node>();
-    public static void main(String[] args) throws Exception {
-        System.out.println("This is Edge class.");
-    }
+abstract public class Edge{
+    int     edgeID = 0;
+    boolean isDirected;
+
+    //directed edges
+    public Node getSource(){ return new Node();}
+    public void setSource(Node source){}
+    public Node getDestination(){ return new Node();}
+    public void setDestination(Node destination){}
+
+    //undirected edges
+    public ArrayList<Node> getNodes(){ return new ArrayList<Node>();}
+    public void setNodes(ArrayList<Node> nodes){}
+
+
 }
 
-class DirectedEdge extends Edge{
-    public DirectedEdge(Node a, Node b){
-        directed = true;
-        start = a;
-        end = b;
-    }
-    public void Info() throws Exception{
-        System.out.println("From "+start.Name+" to "+end.Name);
-    }
-}
 
-class UndirectedEdge extends Edge{
-    public UndirectedEdge(Node a, Node b){
-        directed = false;
-        Nodes.add(a);
-        Nodes.add(b);
-    }
-    public void Info() throws Exception{
-        System.out.println("From "+Nodes.get(0).Name+" to "+Nodes.get(1).Name);
-    }
-}
+
+
