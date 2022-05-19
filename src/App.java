70d746aeeb429820376b22cea309d32fffad6888
Arsalan <arsalan.firoozi@gmail.com>
2022-05-19 14:55:31 +0430

    Q1

--- src/App.java
+++ src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Start....");
+        Q1 obj = new Q1(0);
     }
 }
---
+++ src/Q1.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Q1 implements Runnable {
+    Integer i;
+    Thread thread = null;
+
+    public Q1(Integer j) {
+        i = j;
+        if (i != 50) {
+            j = j + 1;
+            Q1 obj = new Q1(j);
+            thread = new Thread(obj);
+            thread.start();
+        }
+    }
+
+    public void run() {
+        while(thread.isAlive()){
+            
+        }
+        System.out.println("Hello from thread " + i);
+    }
+
+}
