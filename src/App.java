d4e728ce3706a49155c8c9cf5f150c1ace50b9bd
SMAHashemi <35628995+SMAHashemi@users.noreply.github.com>
2022-05-21 15:07:59 -0700

    Q3

--- src/App.java
+++ src/App.java
@@ -1,4 +1,4 @@
-import java.util.ArrayList;
+import java.util.*;
 
 public class App {
     public static void main(String[] args) throws Exception {
@@ -7,16 +7,44 @@ public class App {
         // Q1 obj = new Q1(0);
 
         // Q2
-        Integer i;
-        ArrayList<Thread> threads = new ArrayList<Thread>();
-        for(i=0;i<101;i++){
-            Q2 obj2 = new Q2();
-            Thread thread = new Thread(obj2);
-            threads.add(thread);
+        // Integer i;
+        // ArrayList<Thread> threads = new ArrayList<Thread>();
+        // for(i=0;i<101;i++){
+        //     Q2 obj2 = new Q2();
+        //     Thread thread = new Thread(obj2);
+        //     threads.add(thread);
+        // }
+        // for(i=0;i<101;i++){
+        //     threads.get(i).start();
+        // }
+
+        //Q3
+        int L = 256;
+        int[] A = new int[L];
+        Random ran = new Random();
+        for (int ki = 0; ki < L ; ki++) {
+            // A[i] = i;
+            // A[L-i-1] = i;
+            A[ki] = -200 + ran.nextInt(401);
         }
-        for(i=0;i<101;i++){
-            threads.get(i).start();
+
+        Q3 MS = new Q3();
+        int[] sortedA = MS.mergeSort(A, L);
+
+        System.out.println();
+        for (int ji = 0; ji < L; ji++) {
+            System.out.print(sortedA[ji]+"\t ");
         }
+
+        //check
+        for (int li = 0; li < L-1; li++) {
+            if (sortedA[li+1]<sortedA[li]) {
+                System.out.println("\n\nERROR at i="+li);
+                System.out.println("sortedA["+li+"]="+sortedA[li]);
+                System.out.println("sortedA["+(li+1)+"]="+sortedA[li+1]);
+            }
+        }
+        
 
     }
 }
